526d07b07eab77c05ed37c7648800849a1d5f0ca
Diego Leal <leal.banks@gmail.com>
2017-02-15 22:48:32 -0300

    add rota para buscar frutas pelo id

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,5 +3,6 @@ module.exports = function(app) {
 
     app.post('/fruta/cadastro', controller.postAddFruit);
     app.get('/fruta/busca', controller.getAllFruits);
+    app.get('/fruta/busca/:id', controller.getOneFruit);
     app.post('/fruta/edita', controller.postUpdateFruit);
 };
