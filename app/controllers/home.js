fe27081514adf837eca21c25b7b59a1884ebabb3
Diego Leal <leal.banks@gmail.com>
2017-03-04 18:26:26 -0300

    Refactor no tratamento de erros

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,37 +1,57 @@
 module.exports = function(app) {
-  var fruta = app.models.fruit;
+  var Model = app.models.fruit,
+      fruta = require('../models/mongoose.js')(Model);
 
-    var HomeController = {
-      postAddFruit: function(req, res) {
-        var doc = {
-          nome: req.body.fruta.nome,
-          quantidade: req.body.fruta.quantidade,
-          preco: req.body.fruta.preco.replace(',', '.')
-        };
+  var HomeController = {
+    postAddFruit: function(req, res) {
+      var doc = {
+        nome: req.body.fruta.nome,
+        quantidade: req.body.fruta.quantidade,
+        preco: req.body.fruta.preco.replace(',', '.')
+      };
 
-        fruta.save(doc);
-      },
+      fruta.save(doc, function(err, res) {
+        if(err) throw err;
+
+        console.log('DOCUMENTO INSERIDO COM SUCESSO!\n', res);
+      });
+    },
 
     getAllFruits: function(req, res) {
-      fruta.find({ativo: true}, function(doc) {
+      fruta.find({ativo: true}, function(err, doc) {
+        if(err) throw err;
+
         res.json(doc);
       });
     },
 
     getOneFruit: function(req, res) {
-      fruta.findOne({_id: req.params.id}, function(doc) {
+      fruta.findOne({_id: req.params.id}, function(err, doc) {
+        if(err) throw err;
         res.json(doc);
       });
     },
 
     postUpdateFruit: function(req, res) {
-      var doc = req.body.fruta;
-      fruta.update(doc);
+      var idDoc = req.body.fruta._id,
+          doc = {
+            nome: req.body.fruta.nome,
+            quantidade: req.body.fruta.quantidade,
+            preco: req.body.fruta.preco
+          };
+
+      fruta.update({_id: idDoc}, doc, function(err, res) {
+        if(err) throw err;
+        console.log('FRUTA ATUALIZADA COM SUCESSO!\n', res)
+      });
     },
 
     removeLogicalFruit: function(req, res) {
       var _id = req.body.fruta;
-      fruta.removeLogical(_id);
+      fruta.removeLogical(_id, function(err, res) {
+        if(err) throw err;
+        console.log('FRUTA REMOVIDA COM SUCESSO!\n', res);
+      });
     }
   };
 
