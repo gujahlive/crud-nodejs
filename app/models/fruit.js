5b6784088d6b3b37130d946b03b65c99a35b8c29
Diego Leal <leal.banks@gmail.com>
2017-03-04 18:27:33 -0300

    removed metodos para manipulação do mongo, agora os modelos só terão os schemas

--- app/models/fruit.js
+++ app/models/fruit.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
-
 module.exports = function() {
   var fruitsSchema,
-    Model,
     collectionName = 'fruta';
 
   fruitsSchema = new mongoose.Schema({
@@ -27,57 +25,5 @@ module.exports = function() {
     }
   });
 
-  Model =  mongoose.model(collectionName, fruitsSchema);
-
-  var Fruit = {
-
-    save: function(doc) {
-      fruta = new Model(doc);
-      fruta.save()
-        .then(function(res) {
-          console.log('Documento cadastrada com sucesso! => ', res);
-        }, function(err) {
-          if(err) throw new Error('Erro ao cadastrar o documento:\n', err);
-        });
-    },
-
-    find: function(query, callback) {
-      Model.find(query).exec()
-        .then(function(doc) {
-          callback(doc);
-        }, function(err) {
-          if(err) throw new Error('Erro ao buscar o documento:\n' + err);
-        });
-    },
-
-    findOne: function(query, callback) {
-      Model.findOne(query).exec()
-        .then(function(doc) {
-          callback(doc);
-        }, function(err) {
-          if(err) throw new Error('Erro ao buscar o documento:\n' + err);
-        });
-    },
-
-    update: function(doc) {
-      Model.update({_id: doc._id}, {$set: {nome: doc.nome, quantidade: doc.quantidade, preco: doc.preco}})
-        .then(function(res) {
-          console.log('Documento atualizado com sucesso => ', res);
-        }, function(err) {
-          if(err) throw new Error('Error ao fazer atualização do doc:\n', err);
-        });
-    },
-
-    removeLogical: function(_id) {
-      Model.update({_id: _id}, {$set: {ativo: false}})
-        .then(function(res) {
-          console.log('Documento removido logicamente com sucesso =>', res);
-        }, function(err) {
-          if(err) throw new Error('Erro ao remover logicamente o documento:\n', err);
-        });
-    }
-  };
-  //fim do objeto
-
-  return Fruit;
-}
+  return mongoose.model(collectionName, fruitsSchema);
+};
